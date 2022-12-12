272e9f1e714bb38b2bf12f90ecde77f3a7fa9c54
Ashwin S <ashwin@Ashwins-MacBook-Pro.local>
2022-12-12 15:33:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
+import { Button } from '@mui/material';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button clor='primary' size='small' variant='contained'>
+        submit
+      </Button>
+      <br /><br />
+      <Button clor='primary' size='medium' variant='contained'>
+        cancel
+      </Button>
+      <br /><br />
+      <Button clor='primary' size='Large' variant='contained'>
+        go back
+      </Button>
+      <br /><br />
     </div>
   );
 }
